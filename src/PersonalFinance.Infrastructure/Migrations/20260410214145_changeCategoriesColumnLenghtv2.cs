e9420887898ad2818e2a13a6d4589fc2ba32c002
Caique Araujo <c3caique.araujo@carglass.com.br>
2026-04-10 19:47:56 -0300

    feat(misc): Ajustes de layout e ajustes de tamanhos de coluna da coluna icon da abela categorias

---
+++ src/PersonalFinance.Infrastructure/Migrations/20260410214145_changeCategoriesColumnLenghtv2.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalFinance.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeCategoriesColumnLenghtv2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Icon",
+                table: "Category",
+                type: "nvarchar(1000)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Icon",
+                table: "Category",
+                type: "nvarchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ src/PersonalFinance.Infrastructure/Migrations/20260410221300_changeCategoriesColumnLenghtToMax.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalFinance.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeCategoriesColumnLenghtToMax : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Icon",
+                table: "Category",
+                type: "nvarchar(MAX)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Icon",
+                table: "Category",
+                type: "nvarchar(1000)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(MAX)",
+                oldNullable: true);
+        }
+    }
+}
